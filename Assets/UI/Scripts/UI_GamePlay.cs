12385f83669fc829c2f616e863290d84aa1ba954
Arianabetsi <arianabetsi2005@gmail.com>
2024-06-13 08:27:17 +0100

    1.0.5
    Creación de la escena inicial

---
+++ Assets/UI/Scripts/UI_GamePlay.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine.Profiling;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UI_GamePlay : MonoBehaviour
+{
+
+    // Pantalla de Inicio
+    [SerializeField] GameObject Buttonstart;
+    [SerializeField] GameObject ButtonSettings;
+    [SerializeField] GameObject Logo;
+
+    // Pantalla de juego
+    [SerializeField] GameObject crossBlue1;
+    [SerializeField] GameObject crossBlue2;
+    [SerializeField] GameObject crossBlue3;
+    [SerializeField] GameObject crossRed1;
+    [SerializeField] GameObject crossRed2;
+    [SerializeField] GameObject crossRed3;
+    [SerializeField] GameObject crossRedMidle;
+    [SerializeField] GameObject crossRed2Midle;
+    [SerializeField] GameObject crossRed3Midle;
+    [SerializeField] GameObject ready;
+    [SerializeField] GameObject goooooooo;
+    [SerializeField] GameObject Best;
+    [SerializeField] GameObject GameOver;
+    [SerializeField] GameObject ImageSandia;
+    [SerializeField] GameObject NumberPoints;
+    [SerializeField] GameObject Record;
+
+    // Pantalla de derrota
+    [SerializeField] GameObject ButtonRetry;
+    [SerializeField] GameObject ButtonQuit;
+    [SerializeField] GameObject PanelGameOver;
+    [SerializeField] GameObject Score;
+    [SerializeField] GameObject NumberScore;
+
+    // Pantalla de Settings
+    [SerializeField] GameObject PanelCredits;
+    [SerializeField] GameObject PanelAudio;
+    [SerializeField] GameObject ButtonBack;
+    [SerializeField] GameObject ButtonAudioOn;
+    [SerializeField] GameObject ButtonAudioOff;
+    [SerializeField] GameObject ButtonEffectsOn;
+    [SerializeField] GameObject ButtonEffectsOff;
+    [SerializeField] GameObject Audio;
+    [SerializeField] GameObject PrefabBombBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Aquí puedes inicializar tu juego
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aquí puedes manejar las actualizaciones por frame
+    }
+
+
+
+   
+}
