ab6ef7d2a5607ec65b1179c77f3f0b31a10dca4b
Arianabetsi <arianabetsi2005@gmail.com>
2024-06-14 09:31:55 +0100

    Arreglos en la UI settings y inicial
    Hecho el Leantween del MenuInicial y del Settings

--- Assets/UI/Scripts/InitialMenu.cs
+++ Assets/UI/Scripts/InitialMenu.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class InitialMenu : MonoBehaviour
 {
+    [SerializeField] GameObject Logo, ButtonStart, ButtonSettings, MeetToStart, MeetToSettings;
+    [SerializeField] GameObject Opciones;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       MenuInicialCame();
+        Opciones.gameObject.SetActive(false);
+        this.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+    public void MenuInicialCame()
+    {
+        LeanTween.moveLocal(Logo, new Vector2(0, 200), 0.5f).setEase(LeanTweenType.linear);
+        LeanTween.scale(ButtonStart, Vector3.one, 0.5f);
+        LeanTween.scale(ButtonSettings, Vector3.one, 0.5f);
+        LeanTween.scale(MeetToStart, Vector3.one, 0.5f);
+        LeanTween.scale(MeetToSettings, Vector3.one, 0.5f);
+    }
+
+    //funciones para los botones
+    public void SettingsButton()
+    {
+        LeanTween.moveLocal(Logo, new Vector2(0, 1164), 1f).setEase(LeanTweenType.linear).setOnComplete(() => 
+        { 
+            this.gameObject.SetActive(false);
+            Opciones.gameObject.SetActive(true);
+
+        }); ;
+        LeanTween.scale(ButtonStart, Vector3.zero, 0.5f);
+        LeanTween.scale(ButtonSettings, Vector3.zero, 0.5f);
+        LeanTween.scale(MeetToStart, Vector3.zero, 0.5f);
+        LeanTween.scale(MeetToSettings, Vector3.zero, 0.5f);
+       
+    }
+
 }
--- Assets/UI/Scripts/SettingsScreen.cs
+++ Assets/UI/Scripts/SettingsScreen.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SettingsScreen : MonoBehaviour
 {
+    [SerializeField] GameObject PanelCredits;
+    [SerializeField] GameObject PanelAudio;
+    [SerializeField] GameObject Back;
+    [SerializeField] GameObject ButtonAudioOn;
+    [SerializeField] GameObject ButtonAudioOff;
+    [SerializeField] GameObject ButtonEffectsOn;
+    [SerializeField] GameObject ButtonEffectsOff;
+    [SerializeField] GameObject Audio;
+    [SerializeField] GameObject PrefabBombBack;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MenuSettingsCame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(PanelAudio.transform.position.y);
+    }
+
+    public void MenuSettingsCame()
+    {
+        LeanTween.moveLocal(PanelCredits, new Vector3(-300,0), 1f);  
+        LeanTween.moveLocalX(PanelAudio, 600, 1f);
+        LeanTween.scale(Back, Vector3.one, 1f);
+        LeanTween.scale(PrefabBombBack, Vector3.one, 1f);
     }
 }
