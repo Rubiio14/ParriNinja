d0749b78c7c83ae2de21c402c360036955e2f15e
Rubiio14 <crisperezdocumentos@gmail.com>
2024-06-17 16:08:56 +0100

    Score_Manager arreglado

--- Assets/Scripts/Score_Manager.cs
+++ Assets/Scripts/Score_Manager.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 
 public class Score_Manager : MonoBehaviour
 {
-    int actualMoment = 0;
-    int m_Carnes;
+    int m_NCarnes = 0;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < RythmManager.instance.gameLoop.Length; i++)
         {
-            for (int j = 0; j < RythmManager.instance.gameLoop[j].Length; j++)
-                m_Carnes++;
-            i++;
+            for (int j = 0; j < RythmManager.instance.gameLoop[j].m_objectTypes.Length; j++)
+            {
+                Debug.Log("Suma 1 a " + m_NCarnes + " Carnes");
+                m_NCarnes++;
+                
+            }
+            
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (m_NCarnes == 0)
+        { 
+            Debug.Log("Fin");
+        }
     }
+
 }
