d26c063321b31e7425d9beb99b1726c5329c7182
jrguez03 <jokorguez@gmail.com>
2024-06-17 12:38:16 +0100

    script de prueba para el trail funcional

--- Assets/VFX/VFX Scripts/VFX_trail_cursor.cs
+++ Assets/VFX/VFX Scripts/VFX_trail_cursor.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class VFX_trail_cursor : MonoBehaviour
 {
+    private Camera mainCamera;
+    [SerializeField] TrailRenderer trail;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        trail = GetComponent<TrailRenderer>();
+        trail.emitting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            trail.emitting = true; 
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f; 
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+            transform.position = worldPosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            trail.emitting = false;
+        }
     }
 }
