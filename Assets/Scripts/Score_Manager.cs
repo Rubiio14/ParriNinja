01a8f2d66272ff1959488d4b99e85a0edf971745
Rubiio14 <147033219+Rubiio14@users.noreply.github.com>
2024-06-17 14:48:50 +0100

    actualización del Score_Manager

--- Assets/Scripts/Score_Manager.cs
+++ Assets/Scripts/Score_Manager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class Score_Manager : MonoBehaviour
 {
-    
+    int actualMoment = 0;
+    int m_Carnes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < RythmManager.instance.gameLoop.Length; i++)
+        {
+            for (int j = 0; j < RythmManager.instance.gameLoop[j].Length; j++)
+                m_Carnes++;
+            i++;
+        }
     }
 
     // Update is called once per frame
@@ -18,4 +24,3 @@ public class Score_Manager : MonoBehaviour
         
     }
 }
-
