511b891b243decc6581ef92c5e22451aa2e774dd
Sergiojak <sergiohg.jak@gmail.com>
2024-06-18 19:36:08 +0100

    Ajustes Prefabs hueso y carne

--- Assets/Scripts/HuesoBehaviour.cs
+++ Assets/Scripts/HuesoBehaviour.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class HuesoBehaviour : MonoBehaviour
 {
+    Vector3 m_RotacionHueso;
+    private void Start()
+    {
+        m_RotacionHueso = new Vector3(Random.Range(0.05f, 0.25f), Random.Range(0.05f, 0.25f), Random.Range(0.05f, 0.25f));
+
+    }
+    private void Update()
+    {
+        gameObject.transform.Rotate(m_RotacionHueso);
+
+    }
     public void Hueso()
     {
         Health_Manager.instance.RestaVida();
