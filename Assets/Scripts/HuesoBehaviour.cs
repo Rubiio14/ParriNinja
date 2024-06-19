b722415ba778217a084a8d86df2de68f0abb34c0
Sergio <sergiohg.jak@gmail.com>
2024-06-19 11:53:19 +0100

    Despawn Hueso

--- Assets/Scripts/HuesoBehaviour.cs
+++ Assets/Scripts/HuesoBehaviour.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class HuesoBehaviour : MonoBehaviour
 {
     Vector3 m_RotacionHueso;
+
+    //Despawn
+    public float m_DespawnTimer = 0f;
+    bool m_NeedTimer = false;
     private void Start()
     {
         m_RotacionHueso = new Vector3(Random.Range(0.05f, 0.25f), Random.Range(0.05f, 0.25f), Random.Range(0.05f, 0.25f));
@@ -13,11 +17,29 @@ public class HuesoBehaviour : MonoBehaviour
     private void Update()
     {
         gameObject.transform.Rotate(m_RotacionHueso);
-
+        if (m_NeedTimer)
+        {
+            m_DespawnTimer += Time.deltaTime;
+        }
+        if (m_DespawnTimer >= 3)
+        {
+            gameObject.SetActive(false);
+            m_DespawnTimer = 0;
+            m_NeedTimer = false;
+        }
     }
     public void Hueso()
     {
         Health_Manager.instance.RestaVida();
         Health_Manager.instance.Defeat();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Despawner")
+        {
+            m_NeedTimer = true;
+          
+        }
     }
 }
